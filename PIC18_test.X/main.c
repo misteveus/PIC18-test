31a29a2fd5e524397d20171054dfee9b6804d9f1
Steven Parker <steven.parker@oit.edu>
2021-10-08 20:55:15 -0700

    add init functions

--- PIC18_test.X/main.c
+++ PIC18_test.X/main.c
@@ -1,10 +1,37 @@
 #include "devcfg.h"
 #include <xc.h>
 
+void init(void);
+void uart_init(void);
+void spi_init(void);
+void lora_init(void);
+
 int main(void)
 {
+    init();
+    
     while(1)
     {
         
     }
+}
+
+void init(void)
+{
+    
+}
+
+void uart_init(void)
+{
+    
+}
+
+void spi_init(void)
+{
+    
+}
+
+void lora_init(void)
+{
+    
 }
