02994826ca095715c39bca597da1391978e1d5a1
Steven Parker <steven.parker@oit.edu>
2022-01-16 05:06:16 -0800

    Add heartbeat function that toggles port RA3

--- PIC18_test.X/main.c
+++ PIC18_test.X/main.c
@@ -6,13 +6,18 @@ void uart_init(void);
 void spi_init(void);
 void lora_init(void);
 
+void heartbeat(void);
+
 int main(void)
 {
     init();
     
     while(1)
     {
-        
+        heartbeat();
+    
+        // wait
+        for (int i = 0; i < 10000; i++);
     }
 }
 
@@ -21,6 +26,9 @@ void init(void)
     uart_init();
     spi_init();
     lora_init();
+    
+    // set port RA3 to output 
+    TRISA &= ~_TRISA_TRISA4_MASK;
 }
 
 void uart_init(void)
@@ -36,4 +44,18 @@ void spi_init(void)
 void lora_init(void)
 {
     
+}
+
+void heartbeat(void)
+{
+    
+    // turn on 
+    PORTA |= _PORTA_RA4_MASK;
+    
+    // wait
+    for (int i = 0; i < 10000; i++);
+    
+    // turn off
+    PORTA &= ~_PORTA_RA4_MASK;
+   
 }
